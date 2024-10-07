6cffc93b00856ed0a9e367571ef72aca5dcb88cc
KzAshimo <good.ashimo@gmail.com>
2024-10-07 23:16:46 +0900

    express react 連結

--- frontend/src/LoginForm.tsx
+++ frontend/src/LoginForm.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
+import axios from 'axios'
 
 
 const LoginForm = () =>{
     const [username, setUsername] = useState('');
     const [passwaord, setPasswaord] = useState('');
+    const [number,setNumber] = useState();
     const [error, setError] = useState<string | nuull>(null)
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post("http://localhost:3000/login", {
+                username,
+                password,
+            });
+
+            console.log("Login successful", response.data);
+        } catch (error) {
+            setError("Invalid credentials or server error");
+        }
+    };
+
 
 
     return(
